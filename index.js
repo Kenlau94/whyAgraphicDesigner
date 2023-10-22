64487568203abaa193adfd053a1c6dae88dc7f6a
Kenlau94 <kenlauwork94@gmail.com>
2023-10-22 12:03:06 -0400

    +inquirer function for prompts

--- index.js
+++ index.js
@@ -7,4 +7,30 @@ const { Triangle, Circle, Square } = require("./lib/shapes");
 //Importing the generateLogo file
 const generateLogo = require("./lib/generateLogo");
 
+//Declaring colors to use later in choices
+const red = "#FF0000";
+const blue = "#0000FF";
+const green = "#008000";
+const white = "#FFFFFF";
+const black = "#000000";
+const pink = "#ff69b4";
+
+//Starting inquirer.js in the terminal
+const startInquirer = async () => {
+  console.log("It's Time! Generating your SVG logo!");
+
+  try {
+    const userInputs = await inquirer.prompt([]);
+    //generate the logo using the data from above and the generateLogo.js file
+    const results = generateLogo(userInputs);
+
+    //write the file to be in the ./examples folder
+    await writeFile("./examples/logo.svg", results);
+
+    console.log("Your SVG logo is alive!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 startInquirer();
