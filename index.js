aea280de90d90c01c7c2e22eeed41a6748c04498
Kenlau94 <kenlauwork94@gmail.com>
2023-10-22 12:18:33 -0400

    added questions for inq

--- index.js
+++ index.js
@@ -20,7 +20,50 @@ const startInquirer = async () => {
   console.log("It's Time! Generating your SVG logo!");
 
   try {
-    const userInputs = await inquirer.prompt([]);
+    const userInputs = await inquirer.prompt([
+      //First question to ask the shape the user wants to use for the logo
+      {
+        name: "shape",
+        type: "list",
+        message:
+          "What shape would you like your logo to be? Please choose one of the following:",
+        choices: [Circle, Square, Triangle],
+      },
+      //Second question for the color of the logo
+      {
+        name: "shapeColor",
+        type: "list",
+        message:
+          "Please select the color you would like your logo shape to be:",
+        choices: ["Red", "Blue", "Green", "White", "Black", "Pink"],
+      },
+      //Third question for the logo for characters
+      {
+        name: "text",
+        type: "input",
+        message:
+          "Please enter the text you would like to display inside the logo (1-3 characters only):",
+        validate: (letters) => {
+          //Adding a check to validate that the text entered is not longer than 3 characters
+          if (letters && letters.length <= 3) {
+            return true;
+          } else {
+            console.log(
+              "Please only enter a max of 3 characters for your logo"
+            );
+            return false;
+          }
+        },
+      },
+      //Fourth question for the logo for color of the logo
+      {
+        name: "textColor",
+        type: "list",
+        message:
+          "Please select the color you would like your text color to be:",
+        choices: ["Red", "Blue", "Green", "White", "Black", "Pink"],
+      },
+    ]);
     //generate the logo using the data from above and the generateLogo.js file
     const results = generateLogo(userInputs);
 
